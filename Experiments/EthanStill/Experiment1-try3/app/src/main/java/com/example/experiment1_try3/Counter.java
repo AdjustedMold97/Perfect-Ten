b76062b719127a9b2c767b94febe7e7d220df9c4
estill <edstill@iastate.edu>
2021-09-05 15:48:13 -0500

    Adding basic button

---
+++ Experiments/EthanStill/Experiment1-try3/app/src/main/java/com/example/experiment1_try3/Counter.java
@@ -0,0 +1,14 @@
+package com.example.experiment1_try3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Counter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.counter);
+    }
+}

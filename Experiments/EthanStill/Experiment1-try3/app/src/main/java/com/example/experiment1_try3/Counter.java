4bf893df5f96ff11d43587775c7ac779560280d4
estill <edstill@iastate.edu>
2021-09-07 14:14:43 -0500

    finishing up experiment1

--- Experiments/EthanStill/Experiment1-try3/app/src/main/java/com/example/experiment1_try3/Counter.java
+++ Experiments/EthanStill/Experiment1-try3/app/src/main/java/com/example/experiment1_try3/Counter.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Counter extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +17,30 @@ public class Counter extends AppCompatActivity {
         setContentView(R.layout.counter);
 
 
-//        Button Home = findViewById(R.id.activity_main_button_counter);
-//
-//        Home.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                // where write logic for button
-//                startActivity(new Intent(view.getContext(),MainActivity.class));
-//            }
-//        });
+        Button Home = findViewById(R.id.Home);
+        Button GoUp = findViewById(R.id.GoUp);
+        TextView counterText = findViewById(R.id.CounterText);
+
+        Integer[] value = {0};
+
+        Home.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // where write logic for button
+                startActivity(new Intent(view.getContext(),MainActivity.class));
+            }
+        });
+
+        GoUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                value[0]++;
+                counterText.setText(value[0].toString());
+            }
+        });
+
+
     }
 }

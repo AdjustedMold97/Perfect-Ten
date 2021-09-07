885ce2f9a8e8ba039c79c4d70b6381125fc83256
estill <edstill@iastate.edu>
2021-09-07 13:47:49 -0500

    saving progress on Exp1

--- Experiments/EthanStill/Experiment1-try3/app/src/main/java/com/example/experiment1_try3/Counter.java
+++ Experiments/EthanStill/Experiment1-try3/app/src/main/java/com/example/experiment1_try3/Counter.java
@@ -2,7 +2,10 @@ package com.example.experiment1_try3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Counter extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class Counter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.counter);
+
+
+//        Button Home = findViewById(R.id.activity_main_button_counter);
+//
+//        Home.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // where write logic for button
+//                startActivity(new Intent(view.getContext(),MainActivity.class));
+//            }
+//        });
     }
 }
--- Experiments/EthanStill/Experiment1-try3/app/src/main/java/com/example/experiment1_try3/MainActivity.java
+++ Experiments/EthanStill/Experiment1-try3/app/src/main/java/com/example/experiment1_try3/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.experiment1_try3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button counter = findViewById(R.id.activity_main_button_counter);
+
+        counter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            // where write logic for button
+                startActivity(new Intent(view.getContext(),Counter.class));
+            }
+        });
+
+
+
     }
 }

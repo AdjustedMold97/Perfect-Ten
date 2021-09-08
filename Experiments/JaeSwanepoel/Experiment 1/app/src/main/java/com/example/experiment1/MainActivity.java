9120139d0620707a954087c832b4662b0a94ece9
JaeSwanepoel <jaeswan@iastate.edu>
2021-09-08 13:09:54 -0500

    final update for experiment 1

--- Experiments/JaeSwanepoel/Experiment 1/app/src/main/java/com/example/experiment1/MainActivity.java
+++ Experiments/JaeSwanepoel/Experiment 1/app/src/main/java/com/example/experiment1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.experiment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,8 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button screen1button = findViewById(R.id.screen1button);
+        Button counterButton = findViewById(R.id.counter_button);
 
+        counterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(view.getContext(), CounterActivity.class));
+
+            }
+        });
     }
 
 }

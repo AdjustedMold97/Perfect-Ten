3aca9eef94805e00dd9727683c030b788775d0d4
estill <edstill@iastate.edu>
2021-09-26 15:12:30 -0500

    front end basic home screen

---
+++ Frontend/app/src/main/java/com/example/homescreen/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.homescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button friend = findViewById(R.id.Friends);
+
+        friend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(view.getContext(),FriendsScreen.class));
+
+            }
+        });
+
+        Button settings = findViewById(R.id.Settings);
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(view.getContext(),SettingsScreen.class));
+
+            }
+        });
+
+        Button home = findViewById(R.id.Home);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(view.getContext(),MainActivity.class));
+
+            }
+        });
+    }
+
+
+
+}
---
+++ Frontend/app/src/main/java/com/example/homescreen/SettingsScreen.java
@@ -0,0 +1,28 @@
+package com.example.homescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SettingsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_screen);
+    }
+
+//    Button home = findViewById(R.id.Home);
+//
+//        home.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//
+//            startActivity(new Intent(view.getContext(),MainActivity.class));
+//
+//        }
+//    });
+}

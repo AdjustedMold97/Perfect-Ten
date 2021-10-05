1426d68aed01e5f1c2d4a903a2e5a7c2b7f00977
Jrbarmen <jrbarmen@iastate.edu>
2021-10-04 20:13:00 -0500

    post delete experiment

--- Backend/usertest/src/main/java/com/example/Posts/PostController.java
+++ Backend/usertest/src/main/java/com/example/Posts/PostController.java
@@ -2,6 +2,7 @@ package com.example.Posts;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +45,11 @@ public class PostController {
         postRepository.save(request);
         return postRepository.findById(id);
     } 
-      
+    /*
+    @DeleteMapping(path = "/posts/{id}")
+    String deletePost(@PathVariable long id){
+    	postRepository.deleteById(id);
+    	return success;
+    }
+    */
 }

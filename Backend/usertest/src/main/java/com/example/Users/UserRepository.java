53a49b53f48f61e1815a690bcb78835ee80562d2
asweiger <asweiger@iastate.edu>
2021-10-04 20:05:41 -0500

    Fixed deleteUser(), now works properly

--- Backend/usertest/src/main/java/com/example/Users/UserRepository.java
+++ Backend/usertest/src/main/java/com/example/Users/UserRepository.java
@@ -1,8 +1,11 @@
 package com.example.Users;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findById(int id);
+    @Transactional
     void deleteById(int id);
 }

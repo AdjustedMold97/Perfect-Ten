32fb38c5665f6d9d49dbe1f7bc4310a9a6c03c91
JaeSwanepoel <jaeswan@iastate.edu>
2021-10-05 12:31:17 -0500

    added code to parse JSON data

--- Frontend/app/src/main/java/com/example/homescreen/LoginScreen.java
+++ Frontend/app/src/main/java/com/example/homescreen/LoginScreen.java
@@ -23,11 +23,13 @@ import com.example.homescreen.app.AppController;
 import org.json.*;
 
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class LoginScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
@@ -62,32 +64,47 @@ public class LoginScreen extends AppCompatActivity {
             /*
              * <username> stores the entered username
              * <password> stores the entered password
+             * <final
              * <login_info> stores the username and password inputs
              *              in a JSONObject
+             * <response_text> the data received in the JSON response
+             * <response_reader> Scanner to parse JSON response data
+             * <temp> used to assist in method functionality
+             * <sub_scanner> used to assist in method functionality
              *
              * - Jae Swanepoel
              */
             String username;
             String password;
-            String finalUrl;
+            String final_url;
             JSONObject login_info;
+            String response_text;
+            Scanner response_reader;
+            String temp;
+            Scanner sub_scanner;
 
 
             @Override
             public void onClick(View view) {
 
+                /*
+                 * Assigning username and password values
+                 * from user input
+                 *
+                 * - Jae Swanepoel
+                 */
                 username = String.valueOf(username_input.getText());
                 password = String.valueOf(password_input.getText());
 
                 //creating the final url based on username - Jae Swanepoel
-                finalUrl = url + username;
-
-                login_info = new JSONObject();
+                final_url = url + username;
 
                 /*
                  * adding <username> and <password> into <login_info>
                  * - Jae Swanepoel
                  */
+                login_info = new JSONObject();
+
                 try {
                     login_info.put("username", username);
                     login_info.put("password", password);
@@ -96,20 +113,66 @@ public class LoginScreen extends AppCompatActivity {
                 }
 
                 /*
-                 * Posts a JSON Object <login_info> to the server url
+                 * Creating the Request to add to the RequestQueue
                  * - Jae Swanepoel
                  */
-                JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, finalUrl, login_info, new Response.Listener<JSONObject>() {
+                JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, final_url, login_info,
+
+                    new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
 
-                        Log.d(tag_json_obj, response.toString());
                         /*
-                         * TODO parse response data for password - JAE
-                         * authenticate baBy
+                         * Assigning the JSON data value to the
+                         * string we will parse
+                         *
+                         * - Jae Swanepoel
                          */
+                        response_text = response.toString();
 
+                        //logging response
+                        Log.d(tag_json_obj, response_text);
+
+                        //trimming the brackets off the string
+                        response_text = response_text.substring(1, response_text.length() - 2);
+
+                        //initializing response_reader
+                        //assigning comma as delimiter
+                        response_reader = new Scanner(response_text);
+                        response_reader.useDelimiter(",");
+
+                        /*
+                         * Going through the JSON data until
+                         * we find the password field.
+                         *
+                         * - Jae Swanepoel
+                         */
+                        while (response_reader.hasNext()) {
+
+                            temp = response_reader.next();
+
+                            //identifying the password field
+                            if (temp.substring(1,8) == "password") {
+
+                                sub_scanner = new Scanner(temp.substring(10));
+                                sub_scanner.useDelimiter("\"");
+
+                                //skipping the first substring,
+                                //which should just be " : "
+                                sub_scanner.next();
+
+                                if (sub_scanner.next() == password) {
+
+                                    response_reader.close();
+                                    sub_scanner.close();
+
+                                    startActivity(new Intent(view.getContext(),MainActivity.class));
+                                }
+                            }
+                        }
+
+                        response_reader.close();
                         /*
                          * response "logs a user in if information is correct"
                          * will send user to their home page
@@ -119,12 +182,9 @@ public class LoginScreen extends AppCompatActivity {
 
                         System.out.println(response.toString());
 
-                        //if (password == )
-                            startActivity(new Intent(view.getContext(),MainActivity.class));
+                    }},
 
-                    }
-
-                }, new Response.ErrorListener() {
+                    new Response.ErrorListener() {
 
                     @Override
                     public void onErrorResponse(VolleyError error){

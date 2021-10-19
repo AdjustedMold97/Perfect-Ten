bfcb13ee4a42709ff546b6b08e98a5926ca31da5
JaeSwanepoel <jaeswan@iastate.edu>
2021-10-19 18:33:11 -0500

    minor changes

--- Frontend/app/src/main/java/com/example/homescreen/net_utils/Const.java
+++ Frontend/app/src/main/java/com/example/homescreen/net_utils/Const.java
@@ -7,5 +7,5 @@ public class Const {
     public static final String POST_LIST_URL = "http://coms-309-060.cs.iastate.edu:8080/posts/list";
     public static final String USER_POST_URL_1 = "http://coms-309-060.cs.iastate.edu:8080/user/";
     public static final String USER_POST_URL_2 = "/posts/list";
-    public static final String SIGN_UP_URL = "nothing for now"; //TODO
+    public static final String SIGN_UP_URL = "http://coms-309-060.cs.iastate.edu:8080/user/new";
 }

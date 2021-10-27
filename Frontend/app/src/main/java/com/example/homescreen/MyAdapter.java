01a82606b5bdb09b8bb8ce5b4d29d951254aa603
estill <edstill@iastate.edu>
2021-10-27 18:46:48 -0500

    inifinate scroll adapter

--- Frontend/app/src/main/java/com/example/homescreen/MyAdapter.java
+++ Frontend/app/src/main/java/com/example/homescreen/MyAdapter.java
@@ -18,13 +18,21 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     private List<ListItem> listItems;
     private Context context;
 
+    final int View_TYPE_LOADING = 0;
+    final int VIEW_TYPE_ITEM = 1;
 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.activity_test, parent, false);
+        if(viewType == VIEW_TYPE_ITEM) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.activity_test, parent, false);
 
-        return new ViewHolder(v);
+            return new ViewHolder(v);
+        }
+//        else{
+//            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_loading,parent,false);
+//            return LoadingHolder(view);
+//        }
     }
 
     @Override
@@ -36,6 +44,10 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     }
 
+    public int getItemViewType(int position){
+        return listItems.get(position) == null ? View_TYPE_LOADING : VIEW_TYPE_ITEM;
+
+    }
     @Override
     public int getItemCount() {
         return 0;
@@ -50,12 +62,20 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
         public ViewHolder(@NonNull View itemView){
             super(itemView);
-
+//tvItem = itemView.findByID(R.id.tvitem);
 
 //TODO textViewHead = (TextView) itemView.findViewById(R.id.); don't know what goes in R.id
-            textViewHead = (TextView) itemView.findViewById(R.id.textView4);
-            textViewDesc = (TextView) itemView.findViewById(R.id.textView5);
+//            textViewHead = (TextView) itemView.findViewById(R.id.textView4);
+//            textViewDesc = (TextView) itemView.findViewById(R.id.textView5);
 
+        }
+
+    }
+
+    private class LoadingHolder extends RecyclerView.ViewHolder {
+
+        public LoadingHolder(@NonNull View itemView){
+            super(itemView);
         }
     }
 }

250e665fe4d5e16fc406e69e529b36037408ee65
JaeSwanepoel <jaeswan@iastate.edu>
2021-11-02 17:25:44 -0500

    Add Friend works, merging

--- Frontend/app/src/main/java/com/example/homescreen/Screens/AddFriendScreen.java
+++ Frontend/app/src/main/java/com/example/homescreen/Screens/AddFriendScreen.java
@@ -2,6 +2,7 @@ package com.example.homescreen.Screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -38,6 +39,9 @@ public class AddFriendScreen extends AppCompatActivity {
         responseView = findViewById(R.id.add_friend_response_TextView);
 
         username = findViewById(R.id.editTextUsername);
+
+        Button back = findViewById(R.id.add_friend_back_Button);
+        back.setOnClickListener(view -> startActivity(new Intent(view.getContext(),FriendsScreen.class)));
     }
 
     /*

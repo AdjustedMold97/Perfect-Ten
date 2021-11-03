1818e4d711747a873f81c54a4e25cdbd1b5ac201
estill <edstill@iastate.edu>
2021-11-02 19:09:23 -0500

    making a branch fro mockito

--- Frontend/app/src/main/java/com/example/homescreen/MockitoTesting/FriendsScroll.java
+++ Frontend/app/src/main/java/com/example/homescreen/MockitoTesting/FriendsScroll.java
@@ -1,6 +1,12 @@
 package com.example.homescreen.MockitoTesting;
 
+import java.util.List;
+
 public class FriendsScroll {
 
+    public static void Main(String[] args) {
 
+
+        List mockedlist = new Mock();
+    }
 }

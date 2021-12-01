f9a6672268565bdb309d5f6d12d6f5bdd08c1dac
estill <edstill@iastate.edu>
2021-11-30 23:09:16 -0600

    pop up works

--- Frontend/app/src/main/java/com/example/homescreen/Screens/PostView.java
+++ Frontend/app/src/main/java/com/example/homescreen/Screens/PostView.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.TextView;
 
@@ -30,6 +31,18 @@ public class PostView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_view);
+
+        //==================
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.6));
+
+        //======================
+
 
         getPost();
     }

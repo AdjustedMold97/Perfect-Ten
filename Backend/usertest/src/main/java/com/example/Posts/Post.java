9944001c38f4f3b829c4369561e25ee9a2594fe9
asweiger <asweiger@iastate.edu>
2021-12-02 18:20:38 -0600

    Added getComments() in Post class

--- Backend/usertest/src/main/java/com/example/Posts/Post.java
+++ Backend/usertest/src/main/java/com/example/Posts/Post.java
@@ -2,6 +2,7 @@ package com.example.Posts;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -35,7 +36,7 @@ public class Post {
     private LocalDateTime time;
     
     @ApiModelProperty(notes = "List of comments assosiated with the post",name="comments",required=true,value="comments")
-    private ArrayList<Comment> comments = new ArrayList<Comment>();
+    private List<Comment> comments = new ArrayList<Comment>();
     
     @ManyToOne
     @JoinColumn(name = "user_id")
@@ -180,6 +181,11 @@ public class Post {
     public void setTime() {
     	time = LocalDateTime.now();
     }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
     /**
      * gets a comment stored at input index
      * @param id

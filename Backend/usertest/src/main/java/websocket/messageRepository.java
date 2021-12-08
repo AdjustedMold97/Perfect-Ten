b06e2c160a6f8e4f9bef57a0f8ff51664601c878
Jrbarmen <jrbarmen@iastate.edu>
2021-12-07 19:45:27 -0600

    set up the skeleton with the tourorial 3 as a starting point

--- Backend/usertest/src/main/java/websocket/messageRepository.java
+++ Backend/usertest/src/main/java/websocket/messageRepository.java
@@ -1,11 +1,7 @@
 package websocket;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface messageRepository extends JpaRepository<message, Long> {
-    message findById(int id);
-    @Transactional
-    void deleteById(int id);
+   
 }

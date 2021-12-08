3ce8ca866213db9694da8d065a36f5762582c157
Jrbarmen <jrbarmen@iastate.edu>
2021-12-07 21:46:02 -0600

    oof

--- Backend/usertest/src/main/java/websocket/WebSocket.java
+++ Backend/usertest/src/main/java/websocket/WebSocket.java
@@ -15,11 +15,13 @@ import javax.websocket.server.ServerEndpoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
 
 @Controller
 @ServerEndpoint(value = "/chat/{username}")
+@Component
 
 public class WebSocket {
 	

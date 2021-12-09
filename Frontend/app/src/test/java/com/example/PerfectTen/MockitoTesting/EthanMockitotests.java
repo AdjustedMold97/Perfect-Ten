6897f87f79a8a718a5fbb6f4b86733f18e8240b4
estill <edstill@iastate.edu>
2021-12-08 18:56:29 -0600

    mockito testing

--- Frontend/app/src/test/java/com/example/PerfectTen/MockitoTesting/EthanMockitotests.java
+++ Frontend/app/src/test/java/com/example/PerfectTen/MockitoTesting/EthanMockitotests.java
@@ -1,4 +1,44 @@
 package com.example.PerfectTen.MockitoTesting;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.PerfectTen.Screens.PostCreation;
+import com.example.PerfectTen.app.AppController;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 public class EthanMockitotests {
+
+
+
+        @Mock
+        private View fakeview;
+
+        @Test
+        public void TestCommentAdapter(){
+
+            EditText mockedTitle = mock(EditText.class);
+            when(String.valueOf(mockedTitle.getText())).thenReturn("fake title");
+
+            EditText mockedBody = mock(EditText.class);
+            when(String.valueOf(mockedBody.getText())).thenReturn("fake body");
+
+            assertEquals(String.valueOf(mockedBody.getText()),String.valueOf(mockedTitle.getText()));
+
+            assertEquals("fake Body",mockedBody.getText());
+
+            int mockID = mock(AppController.getPostID());
+
+
+
+
+        }
 }
---
+++ Frontend/app/src/test/java/com/example/PerfectTen/MockitoTesting/ServerMock.java
@@ -0,0 +1,23 @@
+package com.example.PerfectTen.MockitoTesting;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+
+public class ServerMock {
+
+
+    ServerMock mockServer;
+
+
+    @Test
+    public void testPerform() {
+
+        ServerMock server = mock(ServerMock.class);
+
+
+    }
+
+}
